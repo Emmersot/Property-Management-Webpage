39bf5900b952a97d052eb9ccb252f25d2a82afc1
Tyler Emmerson <tyleremmerson0216@gmail.com>
2022-10-02 20:56:23 -0700

    adding image imports.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import Home from './Home'
+import Header from './Header'
+import logo from './companyLogo.png';
 
 function App() {
   return (
     <div className="app">
+      <img src={logo} className="App-companyLogo" alt="Logo" />
+      <Header />
       <Home />
     </div>
   );
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react' ;
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+      <img src={logo} className="App-logo" alt="Logo" />
+    </div>
+  )
+}
+
+export default Header
