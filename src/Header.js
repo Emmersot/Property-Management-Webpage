08dd16f45b6f89b99e64dc8012965e1fb99f8044
Tyler Emmerson <tyleremmerson0216@gmail.com>
2022-10-03 14:49:07 -0700

    create banner and styling.

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,20 @@
 import React from 'react' ;
 import './Header.css'
 
+
 function Header() {
   return (
     <div className='header'>
-      <img src={logo} className="App-logo" alt="Logo" />
+      <img
+        className="header__icon" 
+        src="./companyLogo.png"
+        alt="logo" 
+      />
+      <img
+        className="header__icon2" 
+        src="./companyLogo2.png"
+        alt="logo" 
+      />
     </div>
   )
 }
