168c73389967537274c761a2bda08a5648a2aa39
Tyler Emmerson <tyleremmerson0216@gmail.com>
2022-10-02 20:10:23 -0700

    add Home component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Home from './Home'
 
 function App() {
   return (
     <div className="app">
-      <h1>Rental Management Webpage</h1>
+      <Home />
     </div>
   );
 }
