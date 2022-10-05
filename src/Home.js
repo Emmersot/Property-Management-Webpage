9b8c8f78c97ca7934946c58d50ff21ad094ab93a
Tyler Emmerson <tyleremmerson0216@gmail.com>
2022-10-04 17:09:19 -0700

    resolving css issues.

--- src/Home.js
+++ src/Home.js
@@ -7,23 +7,26 @@ function Home() {
   return (
     <div className='home'>
       <Banner />
-      <div className='home__section'>
-        <Card
-          src="./beach.jpg"
-          title="Online Experiences"
-          description="Unique activities we can do together, led by a world of hosts."
-        />
-        <Card
-          src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w=720"
-          title="Online Experiences"
-          description="Unique activities we can do together, led by a world of hosts."
-        />
-        <Card 
-          src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
-          title="Unique stays"
-          description="Spaces that are more than just a place to sleep."
-        />
-      </div>
+      <div className='wrapper'>
+        <div className='home__section'>
+          
+          <Card
+            src="./beach.jpg"
+            title="Online Experiences"
+            description="Unique activities we can do together, led by a world of hosts."
+          />
+          <Card
+            src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w=720"
+            title="Online Experiences"
+            description="Unique activities we can do together, led by a world of hosts."
+          />
+          <Card 
+            src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
+            title="Unique stays"
+            description="Spaces that are more than just a place to sleep."
+          />
+        </div>
+      
     <div className='home__section'>
         <Card
           src="https://media.nomadicmatt.com/2019/airbnb_breakup3.jpg"
@@ -43,6 +46,7 @@ function Home() {
           description="Superhost with great amenities and a fabolous shopping complex nearby"
           price="£70/night"
         />
+        </div>
     </div>
   </div>
   )
