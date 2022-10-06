313f9ac8112dc41844bf21b8abec5bbcbd1869e8
Tyler Emmerson <tyleremmerson0216@gmail.com>
2022-10-05 21:49:29 -0700

    add photos.

--- src/Home.js
+++ src/Home.js
@@ -13,51 +13,51 @@ function Home() {
                 src="./house1.jpg"
                 title="1 Bedroom apartment"
                 description="Superhost with great amenities and a fabolous shopping complex nearby"
-                price="£70/night"
+                price="$70/night"
             />
             <Card
-                src="./house1.jpg"
+                src="./house2.jpg"
                 title="1 Bedroom apartment"
                 description="Superhost with great amenities and a fabolous shopping complex nearby"
-                price="£70/night"
+                price="$70/night"
             />
             <Card
-                src="./house1.jpg"
+                src="./house3.jpg"
                 title="1 Bedroom apartment"
                 description="Superhost with great amenities and a fabolous shopping complex nearby"
-                price="£70/night"
+                price="$70/night"
             />
             <Card
-                src="./house1.jpg"
+                src="./house4.jpg"
                 title="3 Bedroom Flat in Bournemouth"
                 description="Superhost with a stunning view of the beachside in Sunny Bournemouth"
-                price="£130/night"
+                price="$130/night"
             />
             </div>
             <div className='home__section'>
             <Card
-                src="./house1.jpg"
+                src="./cabin1.jpg"
                 title="3 Bedroom Flat in Bournemouth"
                 description="Superhost with a stunning view of the beachside in Sunny Bournemouth"
-                price="£130/night"
+                price="$130/night"
             />
             <Card
-                src="./house1.jpg"
+                src="./cabin2.jpg"
                 title="Penthouse in London"
                 description="Enjoy the amazing sights of London with this stunning penthouse"
-                price="£350/night"
+                price="$350/night"
             />
             <Card
-                src="./house1.jpg"
+                src="./cabin3.jpg"
                 title="1 Bedroom apartment"
                 description="Superhost with great amenities and a fabolous shopping complex nearby"
-                price="£70/night"
+                price="$70/night"
             />
             <Card
-                src="./house1.jpg"
+                src="./cabin4.jpg"
                 title="3 Bedroom Flat in Bournemouth"
                 description="Superhost with a stunning view of the beachside in Sunny Bournemouth"
-                price="£130/night"
+                price="$130/night"
             />
             </div>
         </div>
